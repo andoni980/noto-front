63ca07cdcd03d7f22d9da49fc3c0d6ae160fb9e5
andoni980 <andonilarrazabal980@gmail.com>
2024-09-08 17:59:08 +0200

    Mejorada función de busqueda, movido botón papelera a menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./notas-list/notas-list.component')
+    loadComponent: () => import('./notas-list/notas-list.component'),
   },
   {
     path: 'papelera',
-    loadComponent: () => import('./notas-papelera-list/notas-papelera-list.component')
+    loadComponent: () => import('./notas-papelera-list/notas-papelera-list.component'),
   },
   {
     path: 'new',
-    loadComponent: () => import('./notas-form/notas-form.component')
+    loadComponent: () => import('./notas-form/notas-form.component'),
   },
   {
     path: ':id/edit',
-    loadComponent: () => import('./notas-form/notas-form.component')
-  }
-
+    loadComponent: () => import('./notas-form/notas-form.component'),
+  },
 ];
