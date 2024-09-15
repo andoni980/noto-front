784e032256658632d9cb352af7f56bd553414cbd
andoni980 <andonilarrazabal980@gmail.com>
2024-09-16 00:58:58 +0200

    Nuevo diseño

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
